257e1c4cbb63849972c556d48a1cd1d64a8ec564
Cavinkumaran M <82271718+Cavinkumaran-m@users.noreply.github.com>
2023-02-25 20:49:27 +0530

    modal for farmers orders

---
+++ english/farmer/farmer_orders.js
@@ -0,0 +1,13 @@
+console.log("asd");
+
+// var myModal = document.getElementById("myModal");
+// var myInput = document.getElementById("myInput");
+
+// myModal.addEventListener("shown.bs.modal", function () {
+//   myInput.focus();
+// });
+
+const confirmed = () => {
+  let status_div = document.getElementById("status");
+  status_div.innerHTML = "<p>Sent&#9989;</p>";
+};
