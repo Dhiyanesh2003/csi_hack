42d2e3282e97fc1f1e15c174a49b135f762f1d42
Dhiyanesh2003 <dhiyanesh7338942092@gmail.com>
2023-02-26 08:31:10 +0530

    place order done and dusted

--- english/consumer/placeOrder.php
+++ english/consumer/placeOrder.php
@@ -14,17 +14,32 @@ if ($conn->connect_error) {
 $sql = "UPDATE cart SET status=2 WHERE cart_id=".$_POST["cart_id"].";";
 $count = 0;
 if ($conn->query($sql) === TRUE) {
-    $count++;
+    $count = 1;
 } else {
   echo "Error updating record: " . $conn->error;
 }
 
-$sql = "SELECT * FROM cart where cart_id=".$_POST["cart_id"].";";
-$result = $conn->query($sql);
+if($count==1){
+    $sql = "SELECT * FROM cart where cart_id=".$_POST["cart_id"].";";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-  }
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $sql1 = "SELECT * FROM products where p_id=".$row['p_id'].";";
+        $result1 = $conn->query($sql1);
+
+
+        if ($result1->num_rows > 0) {
+        // output data of each row
+        while($row1 = $result1->fetch_assoc()) {
+            $sql2 = "UPDATE leaderboard SET score=score+1 WHERE f_id=".$row1['f_id'].";";
+            if ($conn->query($sql2) === TRUE) {
+                $count++;
+            }
+        }
+        }
+    }
+    }
 }
+header("Location: consumer_orders.php");
